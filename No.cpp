b544d804ec533696852f05867f44d46724cb44ca
gmarreta <55359602+gmarreta@users.noreply.github.com>
2019-10-02 20:23:23 -0300

    Create No.cpp

---
+++ No.cpp
@@ -0,0 +1,42 @@
+#include "No.h"
+#include <iostream>
+
+No::No()
+{
+    prox = NULL;
+    id = -1;
+}
+
+No::~No()
+{
+    delete prox;
+}
+
+int No::getId(){
+    return id;
+}
+
+void No::setId(int val){
+    id = val;
+}
+
+No* No::getProx(){
+    return prox;
+}
+
+void No::setProx(No* p){
+    prox = p;
+}
+
+string No::getUser()
+{
+    return user;
+}
+
+void No::setUser(string userr)
+{
+    user = userr;
+}
+
+
+
