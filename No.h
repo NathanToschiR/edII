7063a2c66135dc797dff33dcecd1143a837111a7
gmarreta <55359602+gmarreta@users.noreply.github.com>
2019-10-02 21:57:54 -0300

    Create No.h

---
+++ No.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+#ifndef NO_H
+#define NO_H
+
+class No
+{
+    public:
+        No();
+        ~No();
+        int getId();
+        string getUser();
+        No* getProx();
+        void setId(int val);
+        void setUser(string userr);
+        void setProx(No* p);
+
+    private:
+        string user;
+        int id;
+        No* prox;
+};
+
+#endif // NO_H
