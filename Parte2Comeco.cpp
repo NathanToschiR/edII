59e204db4785081a015fe553b1d10fd6995040a8
Heto27-maker <55469108+Heto27-maker@users.noreply.github.com>
2019-10-06 17:02:20 -0300

    aq meu nego nathan

---
+++ Parte2Comeco.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+struct userId {
+    int idJogo;
+    int id;
+    string TheWho;
+} ;
+
+bool procuraVirgula(string str) {
+    for (int k = 0; k < str.length(); k++) {
+        if (str[k] == ']') {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+bool checagem(string str) {
+    if (str.length() == 0)
+        return 0;
+    else {
+        int cont = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '"')
+            cont++;
+        }
+        if (cont % 2 == 0)
+            return 0;
+        else
+            return 1;
+    }
+}
+
+bool denovo(string str) {
+    int j = 0;
+    for (int k = 0; k < str.length(); k++) {
+        if (str[k] == '[')
+            j++;
+        if (j > 1)
+            return 1;
+    }
+    return 0;
+}
+
+int main() {
+    fstream leitura("games_detailed_info.csv");
+    fstream saida("fuck.txt", ios::app);
+    userId* vetConj = new userId[17063];
+    if(leitura.is_open()) // leitura do arquivo "bgg-13m-reviews.csv"
+    {
+        int j = 0;
+        int aleatorio;
+        string str;
+        getline(leitura, str); // getline para passar pela primeira linha de referência
+        for (j; j < 17062; j++)  // Passagem dos ids para o vetor auxiliar vetConj
+        {
+            getline(leitura, str, ',');
+            vetConj[j].id = stoi(str);
+            cout << vetConj[j].id << "  ";
+            for (int k = 0; k < 16; k++){
+                getline(leitura, str, ',');
+            }
+            getline(leitura, str, ',');
+            if (str[0] == '"' && !procuraVirgula(str)) {
+                getline(leitura, str, ']');
+                getline(leitura, str, ',');
+            }
+            getline(leitura, str, ',');
+            getline(leitura, str, ',');
+            getline(leitura, str, ',');
+            getline(leitura, str, ',');
+            if (str[0] == '"' && !procuraVirgula(str)) {
+                getline(leitura, str, ']');
+                getline(leitura, str, ',');
+            }
+            getline(leitura, str, ',');
+            if (str[0] == '"' && !procuraVirgula(str)) {
+                getline(leitura, str,']');
+                getline(leitura, str, ','); // ver aq
+                str = str + ']';
+            }
+            vetConj[j].TheWho = str;
+            for (int k = 0; k < 7; k++) {
+                getline(leitura, str, ',');
+                if (denovo(str)) {
+                    getline(leitura, str, '"');
+                    getline(leitura, str, ',');
+                }
+                if (str[0] == '"' && !procuraVirgula(str)) {
+                    getline(leitura, str, ']');
+                    getline(leitura, str, ',');
+                }
+            }
+            getline(leitura, str, ']');
+            getline(leitura, str, ',');
+            getline(leitura, str, ',');
+            if (str[0] == '"') {
+                getline(leitura, str, '"');
+                getline(leitura, str, ',');
+                while (checagem(str)) {
+                    getline(leitura, str, '"');
+                    getline(leitura, str, ',');
+                }
+            }
+            getline(leitura, str, ',');
+            cout << str << "  ";
+            if (j == 2704)
+                vetConj[j].idJogo = 132497;
+            if (j == 10833)
+                vetConj[j].idJogo = 84913;
+            if (j != 2704 && j != 10833)
+                vetConj[j].idJogo = stoi(str);
+            getline(leitura, str);
+            cout << endl;
+        }
+    } else {
+        cout << "ERRO: NAO FOI POSSIVEL ABRIR O ARQUIVO .CSV";
+    }
+
+    delete [] vetConj;
+
+    leitura.close();
+} 
