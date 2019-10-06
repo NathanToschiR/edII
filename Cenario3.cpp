3579119d8781637ff6b2dbb80e596ea1808593e1
Heto27-maker <55469108+Heto27-maker@users.noreply.github.com>
2019-10-06 12:19:48 -0300

    Top dos tops

--- Cenario3.cpp
+++ Cenario3.cpp
@@ -41,75 +41,11 @@ void trocar(int* a, int* b)
     *b = aux;
 } // criada função troca para simplificar os códigos
 
-bool checagem(string str) {
-    if (str.length() == 0)
-        return 0;
-    else {
-        int cont = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str[i] == '"')
-            cont++;
-        }
-        if (cont % 2 == 0)
-            return 0;
-        else
-            return 1;
-    }
-}
-
-// -------------------------------------------------------------- QUICKSORT RECURSIVO ---------------------------------------------------------------------
-
-void auxQuickSort(int *vet, int p, int n, int *comp, int* troca)
-{
-    if (n == 2)
-    {
-        if (vet[p] > vet[p + 1])
-        {
-            trocar(&vet[p], &vet[p+1]);
-            (*troca)++;
-        }
-        (*comp)++;
-    }
-    if (n > 2)
-    {
-        int r = vet[p + n - 1];
-        int i = p;
-        for (int j = p; j < p + n - 1; j++)
-        {
-            if (vet[j] <= r)
-            {
-                if (j != i)
-                {
-                    trocar(&vet[i], &vet[j]);
-                    (*troca)++;
-                }
-                i++;
-            }
-        }
-        *comp = *comp + n - 1;
-        vet[p + n - 1] = vet[i];
-        vet[i] = r;
-        (*troca)++;
-        auxQuickSort(vet, p, i - p, comp, troca);
-        auxQuickSort(vet, i+1, p+n-i-1, comp, troca);
-    }
-}
-
-void quickSort(int *vet, int n, int *vetdados) {
-    int comp = 0;
-    int troca = 0;
-    auxQuickSort(vet, 0, n, &comp, &troca);
-    cout << "\nquickSort Trocas: " << troca;
-    cout << "\nquickSort Comparacoes: " << comp << "\n";
-    vetdados[0] = troca + vetdados[0];
-    vetdados[1] = comp + vetdados[1];
-}
-
 // ---------------------------------------------------------------------- COUNTINGSORT ----------------------------------------------------------------------
 
 void countingSort(int* vet, int n)
 {
-    int vetOcorrencias[13170075]; // 272409 maior ID do arquivo
+    int* vetOcorrencias = new int[13170075];
     int* vetAux = new int[n];
 
     for(int i = 0 ; i < 13170075 ; i++)
@@ -139,13 +75,14 @@ void countingSort(int* vet, int n)
     }
 
     delete [] vetAux;
+    delete [] vetOcorrencias;
 }
 
 // --------------------------------------------------------------------INSERTIONSORT -----------------------------------------------------------------------
 
 void insertionSort(int *vet, int n, unsigned int *vetdados) {
-    int comp = 0;
-    int troca = 0;
+    unsigned int comp = 0;
+    unsigned int troca = 0;
     for(int i = 1; i < n ; i++)
     {
         for(int j = i - 1; j >= 0 ; j--)
@@ -168,7 +105,7 @@ void insertionSort(int *vet, int n, unsigned int *vetdados) {
 
 // --------------------------------------------------------------------- MERGESORT -------------------------------------------------------------------------
 
-void Intercala(int *vet, int inicio, int meio, int fim, int *vet1, int* comp) {
+void Intercala(int *vet, int inicio, int meio, int fim, int *vet1, unsigned int* comp) {
     int i = inicio;
     int j = meio + 1;
     int k = 0;
@@ -203,7 +140,7 @@ void Intercala(int *vet, int inicio, int meio, int fim, int *vet1, int* comp) {
     }
 }
 
-void auxMergeSort(int *vet, int inicio, int fim, int *vet1, int* comp) {
+void auxMergeSort(int *vet, int inicio, int fim, int *vet1, unsigned int* comp) {
     if (inicio < fim)
     {
         int meio;
@@ -215,7 +152,7 @@ void auxMergeSort(int *vet, int inicio, int fim, int *vet1, int* comp) {
 }
 
 void MergeSort(int *vet, int n, unsigned int* vetdados) {
-    int comp = 0;
+    unsigned int comp = 0;
     int vet1[n];
     auxMergeSort(vet, 0, n - 1, vet1, &comp);
     cout << "\nquickSort Comparacoes: " << comp << "\n";
@@ -224,7 +161,7 @@ void MergeSort(int *vet, int n, unsigned int* vetdados) {
 
 // ---------------------------------------------------------------------- HEAPSORT -------------------------------------------------------------------------
 
-void heapify(int* vet, int i, int n, int* comp, int* troca) { // Passamos o inteiro n para que não comparemos elementos com indices maiores que n.
+void heapify(int* vet, int i, int n, unsigned int* comp, unsigned int* troca) { // Passamos o inteiro n para que não comparemos elementos com indices maiores que n.
   int l = (2*i) + 1;
   int r = (2*i) + 2;
   int maior = i;
@@ -245,7 +182,7 @@ void heapify(int* vet, int i, int n, int* comp, int* troca) { // Passamos o inte
   }
 }
 
-void BuildMaxHeap(int* vet, int n, int* comp, int* troca) {
+void BuildMaxHeap(int* vet, int n, unsigned int* comp, unsigned int* troca) {
   int i = (n/2) - 1;
   for (i; i >= 0; i--) {
     heapify(vet, i, n, comp, troca);
@@ -253,8 +190,8 @@ void BuildMaxHeap(int* vet, int n, int* comp, int* troca) {
 }
 
 void HeapSort(int* vet, int n, unsigned int *vetdados) {
-    int troca = 0;
-    int comp = 0;
+    unsigned int troca = 0;
+    unsigned int comp = 0;
     BuildMaxHeap(vet, n, &comp, &troca);
     for (int i = n - 1; i >= 2; i--) {
         trocar(&vet[0], &vet[i]);
@@ -290,9 +227,9 @@ int insertionSortQuick(int *vet, int n, int* troca)
     return compar;
 }
 
-void auxQuickSortInsertion(int *vet, int p, int n, int m, int *comp, int* troca)
+void auxQuickSortInsertion100(int *vet, int p, int n, int *comp, int* troca)
 {
-    if (n <= m)
+    if (n <= 100)
         (*comp) = (*comp) + insertionSortQuick(vet, n, troca);
     else
     {
@@ -314,15 +251,15 @@ void auxQuickSortInsertion(int *vet, int p, int n, int m, int *comp, int* troca)
         vet[p + n - 1] = vet[i];
         vet[i] = r;
         (*troca)++;
-        auxQuickSortInsertion(vet, p, i - p, m, comp, troca);
-        auxQuickSortInsertion(vet, i+1, p+n-i-1, m, comp, troca);
+        auxQuickSortInsertion100(vet, p, i - p, comp, troca);
+        auxQuickSortInsertion100(vet, i+1, p+n-i-1, comp, troca);
     }
 }
 
-void quickSortInsertion(int *vet, int n, unsigned int *vetdados, int m) {
+void quickSortInsertion100(int *vet, int n, unsigned int *vetdados) {
     int comp = 0;
     int troca = 0;
-    auxQuickSortInsertion(vet, 0, n, m, &comp, &troca);
+    auxQuickSortInsertion100(vet, 0, n, &comp, &troca);
     cout << "\nquickSort Trocas: " << troca;
     cout << "\nquickSort Comparacoes: " << comp << "\n";
     vetdados[0] = troca + vetdados[0];
@@ -331,28 +268,25 @@ void quickSortInsertion(int *vet, int n, unsigned int *vetdados, int m) {
 
 // ---------------------------------------------------------------------- LEITURA E CHAMADA ----------------------------------------------------------------
 
-void medias2(int grupo, unsigned int *vetDados, int *vetConj, int q, int* vetAux){
-
-    cout << "Entrou no media" << endl;
+void medias3(int grupo, unsigned int *vetDados, int *vetConj, int q, int* vetAux){
     int r;
     int c = 0;
     cout << "Grupo: " << grupo << endl;
 
     for(int i = 0; i < 5; i++){              //ele deve fazer isso 5 vezes, como pedido
-        cout << "Entrou no For " << i+1 << " do media" << endl;
         c = 0;
         while(c < grupo) {                 //preenche o vetor com valores aleatorios referente a entrada
-            r = randomLarge(506542);
-            vetAux[c] = vetConj[r];
-            trocar(&vetConj[r], &vetConj[506542 - c]);
-            c++;
+            r = randomLarge(506541 - c);                  // Para nao haver repeticoes no vetor que iremos ordenar (vetAux), quando geramos um numero
+            vetAux[c] = vetConj[r];                       // aleatorio, passamos o elemento correspondente a essa posicao para vetAux e trocamos este elemento,
+            trocar(&vetConj[r], &vetConj[506541 - c]);    // no vetConj, com o ultimo elemento do vetor e geramos, para o proximo valor de c, um numero 
+            c++;                                          // aleatorio entre 0 e 506541 - c, para q nao coletemos o mesmo valor uma outra vez
         }
         cout << endl;
         auto t1 = std::chrono::high_resolution_clock::now();    //comeca a contar
 
         switch (q) {
             case 1:
-                quickSortInsertion(vetAux, grupo, vetDados, 100);
+                quickSortInsertion100(vetAux, grupo, vetDados);
                 break;
             case 2:
                 insertionSort(vetAux, grupo, vetDados);
@@ -383,39 +317,7 @@ void medias2(int grupo, unsigned int *vetDados, int *vetConj, int q, int* vetAux
     return;
 }
 
-int testeLeitura(fstream* leitura) {
-    string str;
-    getline(*leitura, str, ',');
-    getline(*leitura, str, ',');
-    if (checagem(str))
-    {
-        getline(*leitura, str, ',');
-    }
-    getline(*leitura, str, ',');
-    getline(*leitura, str, ',');
-    if(str[0] == '"')
-    {
-        if (checagem(str))
-        {
-            getline(*leitura, str, '"');
-            getline(*leitura, str, ',');
-            if (checagem(str))
-            {
-                while (checagem(str))
-                {
-                    getline(*leitura, str, '"');
-                    getline(*leitura, str, ',');
-                }
-            }
-        }
-    }
-    getline(*leitura, str, ',');
-    int ID = stoi(str); // -std=c++11
-    getline(*leitura, str);
-    return ID;
-}
-
-void analisaCenario3(int *vetregistro, int q, int* vetAux){
+void analisaCenario3(int *vetConj, int q, int* vetAux){
 
     fstream entrada("entrada.txt");
     fstream saida("saida.txt", ios::app);
@@ -423,30 +325,28 @@ void analisaCenario3(int *vetregistro, int q, int* vetAux){
     string enes, conj;
     int k = 0;
 
-    getline(entrada, conj);                     //descobre quantos conjuntos serão gerados
-    int grupo = stoi(conj);
-    cout << "\nConjunto: " << grupo << endl;
-    int N[grupo];
+    getline(entrada, conj);                     //le o numero de conjuntos que serao gerados a partir do arquivo de entrada
+    int numGrupos = stoi(conj);
+    cout << "\nConjunto: " << numGrupos << endl;
+    int N[numGrupos];
 
-    getline(entrada, enes);             //cria um vetor com a quantidade que cada conjunto deverá ter
-    while(grupo > 0){
+    getline(entrada, enes);             //cria um vetor com a quantidade que cada conjunto deverá ter (N[0] = 1000, N[1] = 5000, ...)
+    while(numGrupos > 0){
         N[k] = stoi(enes);
-        grupo--;
+        numGrupos--;
         k++;
         getline(entrada, enes);
     }
 
-    cout << "Teste1.\n";
-
     int x;
-    unsigned int vetData[3];
+    unsigned int vetDados[3]; // Vetor que armazena as metricas (numero de comparacoes e trocas, tempo)
     int i, j;
 
     saida << "Cenario 3\n";
 
     switch (q) {
         case 1:
-            saida << "QuickSort" << endl;
+            saida << "QuickSort com Insertion m = 100" << endl;
             break;
         case 2:
             saida << "InsetionSort" << endl;
@@ -458,18 +358,19 @@ void analisaCenario3(int *vetregistro, int q, int* vetAux){
             saida << "HeapSort" << endl;
             break;
         case 5:
+            saida << "CountingSort" << endl;
             break;
     }
 
     for(i = 0; i < k; i++){          //para cada conjunto, a analise sera feita
         cout << "\nFor numero " << i << endl;
         for(j = 0; j < 3; j++){
-            vetData[j] = 0;         //zera o vetor de dados
+            vetDados[j] = 0;         //zera o vetor de dados
         }
-        medias2(N[i], vetData, vetregistro, q, vetAux);   //lança na funcao que retornarao as medias
+        medias3(N[i], vetDados, vetConj, q, vetAux);   //lança na funcao que retornarao as medias
         cout << "saiu do media";
-        saida << "Medias do conjunto de " << N[i] << " dados - Trocas: " << vetData[0] << ", Comparacoes: " 
-        << vetData[1] << " e Tempo: " << (double)vetData[2]/1000 << "\n";  //imprime no arquivo
+        saida << "Medias do conjunto de " << N[i] << " dados - Trocas: " << vetDados[0] << ", Comparacoes: " 
+        << vetDados[1] << " e Tempo: " << (double)vetDados[2]/1000 << "\n";  //imprime no arquivo
     }
 
     saida << endl;
@@ -479,41 +380,39 @@ void analisaCenario3(int *vetregistro, int q, int* vetAux){
 void cenario3(int q, int* vetAux)
 {
     fstream leitura("bgg-13m-reviews.csv");
-    int* vetArc = new int[506542];
-
+    int* vetConj = new int[506542];
     if(leitura.is_open()) // leitura do arquivo "bgg-13m-reviews.csv"
     {
         int j = 0;
         int aleatorio;
         string str;
         getline(leitura, str); // getline para passar pela primeira linha de referência
-        for (j; j < 506541; j++)
+        for (j; j < 506541; j++)  // Passagem dos ids para o vetor auxiliar vetConj
         {
             aleatorio = random(0, 25);
-            for (int k = 0; k < aleatorio; k++)
-            {
-                getline(leitura, str);  // joga as linhas fora (antes da linha sortida de cada bloco)
-            }
-            vetArc[j] = testeLeitura(&leitura);
-            for (int k = aleatorio + 1; k < 26; k++)
-            {
-                getline(leitura, str);  // joga as linhas fora (depois da linha sortida de cada bloco)
+            for (int k = 0; k < 26; k++) {
+                if (k == aleatorio) {      // Coleta o id da lina selecionada dentro do grupo
+                    getline(leitura, str, ',');
+                    vetConj[j] = stoi(str);
+                }
+                else
+                    getline(leitura, str); // Joga fora as linhas que n foram selecionadas no grupo
             }
         }
         aleatorio = random(0, 7);
         for (int k = 0; k < aleatorio; k++)
         {
-            getline(leitura, str);  // joga as linhas fora (antes da linha sortida de cada bloco)
+            getline(leitura, str);
         }
-        vetArc[j] = testeLeitura(&leitura);
+        getline(leitura, str, ',');  // Coleta o id da linha selecionada no ultimo grupo, de 8 linhas
+        vetConj[j] = stoi(str);
 
-
-        analisaCenario3(vetArc, q, vetAux);
+        analisaCenario3(vetConj, q, vetAux);
     } else {
         cout << "ERRO: NAO FOI POSSIVEL ABRIR O ARQUIVO .CSV";
     }
 
-    delete [] vetArc;
+    delete [] vetConj;
 
     leitura.close();
 }
@@ -522,7 +421,8 @@ int main()
 {
     srand(time(NULL));
     int* vetAux = new int[500000];
-    cout << "Escolha qual Algoritmo de Ordenacao sera testado.\n Digite '1' para o quicksort recursivo,'2' para o InsertionSort, '3' para o MergeSort, '4' para o HeapSort ou '5' para CountingSort\n";
+    cout << "Escolha qual Algoritmo de Ordenacao sera testado.\n Digite '1' para o QuicksortInsertion,'2' para o InsertionSort, '3' para o MergeSort,";
+    cout << " '4' para o HeapSort ou '5' para CountingSort\n";
     int x, y;
 
     cin >> x;
