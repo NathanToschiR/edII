5e90ea6b925e1fa4db85cef27d9052aca5d79b3c
Heto27-maker <55469108+Heto27-maker@users.noreply.github.com>
2019-11-28 12:26:45 -0300

    sup again

--- arvoreB/ArvoreB.h
+++ arvoreB/ArvoreB.h
@@ -12,13 +12,13 @@ class ArvoreB {
         void insercao(int valor, unsigned int* copias, unsigned int* comp);
         No* BuscaValor(int valor, int* posicao, No* p, bool* result, unsigned int* comp);
         No* libera(No* no);
-        void splitPropagado(No* p, No* novoFilhoEsq, No* novoFilhoDir, int valor, int k);
+        void splitPropagado(No* p, No* novoFilhoEsq, No* novoFilhoDir, int valor, int k, unsigned int* copias, unsigned int* comp);
         void Imprime();
-        int achaMediano(No* p, int val, int* posicaoMed);
+        int achaMediano(No* p, int val, int* posicaoMed, unsigned int* comp);
         void setRelacao(No* pai, int posicao, No* filho);
-        void separaNo(No* p, No* novoFilhoEsq, No* novoFilhoDir, No* novoNoEsq, No* novoNoDir, int val, int k);
-        void primeiroSplit(No* p, int valor);
-        void remove(int valor);
+        void separaNo(No* p, No* novoFilhoEsq, No* novoFilhoDir, No* novoNoEsq, No* novoNoDir, int val, int k, unsigned int* copias, unsigned int* comp);
+        void primeiroSplit(No* p, int valor, unsigned int* copias, unsigned int* comp);
+        void remove(int valor, unsigned int* comp);
 
     private:
         int ProcuraNoPai(No* p);
