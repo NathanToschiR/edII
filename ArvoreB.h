23bf37de6bad60961dcced5f9209e5c282ec27a1
Heto27-maker <55469108+Heto27-maker@users.noreply.github.com>
2019-11-28 11:38:05 -0300

    caminhando sempre

---
+++ ArvoreB.h
@@ -0,0 +1,35 @@
+#ifndef ARVOREB_H
+#define ARVOREB_H
+#include <iostream>
+#include "No.h"
+
+using namespace std;
+
+class ArvoreB {
+    public:
+        ArvoreB();
+        ~ArvoreB();
+        void insercao(int valor, unsigned int* copias, unsigned int* comp);
+        No* BuscaValor(int valor, int* posicao, No* p, bool* result, unsigned int* comp);
+        No* libera(No* no);
+        void splitPropagado(No* p, No* novoFilhoEsq, No* novoFilhoDir, int valor, int k);
+        void Imprime();
+        int achaMediano(No* p, int val, int* posicaoMed);
+        void setRelacao(No* pai, int posicao, No* filho);
+        void separaNo(No* p, No* novoFilhoEsq, No* novoFilhoDir, No* novoNoEsq, No* novoNoDir, int val, int k);
+        void primeiroSplit(No* p, int valor);
+        void remove(int valor);
+
+    private:
+        int ProcuraNoPai(No* p);
+        void verificacaoBraba(No* p);
+        void ConcatenaNosFolhas(No* p, No* r);
+        void ConcatenaNosInternos(No* p, No* r);
+        void RedistribuiNoFolha(No* p, No* r);
+        void RedistribuiNoInterno(No* p, No* r);
+        void PrimeiraRemocao(int valor, No* p, int posicao);
+        No* raiz;
+
+};
+
+#endif // ARVOREB_H
